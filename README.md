0ea07ea0d1fc190c7d38065fea07cb885fdd6d8e
tgoldenphoenix <anhaophamx@gmail.com>
2025-04-22 07:59:30 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Data Structure & Algorithm
+
+## Symmetric Difference
+
+The [argument object](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments) do NOT exist in arrow functions.
---
+++ src/01-symmetric-difference.js
@@ -0,0 +1,35 @@
+function symDiff(arr1, arr2) {
+  const output = [];
+
+  for (const element of arr1) {
+    if (!output.includes(element) && !arr2.includes(element)) {
+      output.push(element);
+    }
+  }
+
+  for (const element of arr2) {
+    if (!output.includes(element) && !arr1.includes(element)) {
+      output.push(element);
+    }
+  }
+
+  return output;
+}
+
+// console.log(symDiff([1, 2, 3], [5, 2, 1, 4]))
+
+function sym() {
+  const arrOfArrs = [...arguments];
+
+  let output = arrOfArrs[0];
+
+  for (let i = 1; i < arrOfArrs.length; i++) {
+    output = symDiff(output, arrOfArrs[i]);
+  }
+
+  return output.sort((a, b) => a-b);
+}
+
+// sym([1, 2, 3], [5, 2, 1, 4]);
+console.log(sym([1, 2, 3], [5, 2, 1, 4]))
+console.log(sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]))
