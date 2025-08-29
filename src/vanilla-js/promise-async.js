1e18cb17c79c04d2675915f52300b314380a57c6
tgoldenphoenix <anhaophamx@gmail.com>
2025-08-29 10:09:06 +0700

    add promise examples

--- src/vanilla-js/promise-async.js
+++ src/vanilla-js/promise-async.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+const fs = require('fs');
 
 // ========== Callbacks ========== 
 
@@ -27,20 +27,68 @@ element.addEventListener("click", function() {
 
 // =============== Promises ===============
 
-// Create a promise
+// run JS online: https://playcode.io/javascript
+
+// Example 01
 const myPromise = new Promise((resolve, reject) => {
   // executor (the producing code, "singer")
-  const rand = Math.floor(Math.random() *2)
+  // const rand = Math.floor(Math.random() *2)
+  const rand = 0
   if(rand === 0) {
-    resolve()
+    // you can resolve() nothin
+    // resolve()
+
+    // or resolve with a return value
+    resolve('resolve string')
   }else {
     reject()
   }
 })
+
 myPromise
-  .then(() => console.log("Success"))
+  // .then(() => console.log("Success"))
+  .then((result) => console.log(result)) // resolve string
   .catch(() => console.log("Something went wrong!!!"))
 
+// =================== example 02 ===================
+function takeYourTime() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      // resolve('resolved hehe')
+
+      // will return an error object instead of a string
+      reject(new Error('Whoops!'));
+    }, 10000); // wait 10 seconds
+  });
+}
+
+async function asyncCall() {
+  console.log('calling async function');
+  const p = takeYourTime();
+  console.log('takeYourTime() returns: ' + p); // return [object Promise]
+  try {
+    // wait 10 seconds, free to do other things
+    console.log('wait 10 seconds for the await')
+    const result = await p;
+    console.log('resolve() returns: ' + result); // string 'resolved hehe'
+    console.log('data type: ' + typeof result); // string
+  } catch (err) {
+    console.log('reject() returns: ' + err);
+    console.log('data type: ' + typeof err); // error object
+  }
+}
+asyncCall()
+console.log("While waiting 10 seconds, I can do other things :D")
+
+// =================== example 03 ===================
+let promise = loadScript("https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.js");
+
+promise.then(
+  script => alert(`${script.src} is loaded!`),
+  error => alert(`Error: ${error.message}`)
+);
+
+promise.then(script => alert('Another handler...'));
 /*==========================*/
 
 // This function return a promise object
